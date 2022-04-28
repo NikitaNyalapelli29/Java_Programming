1980d8b57c110c1854ae41199415f6be8c39e0ef
Nikita Nyalapelli <85953042+NikitaNyalapelli29@users.noreply.github.com>
2022-04-28 20:09:51 +0530

    Create sum of two numbers.java

---
+++ sum of two numbers.java
@@ -0,0 +1,11 @@
+package basic;
+
+public class Practice {
+    public static void main(String[] args) {
+      int num1=34;
+      int num2=22;
+      int sum = num1+num2;
+      System.out.println("The sum of two numbers is: ");
+      System.out.println(sum);
+    }
+}
