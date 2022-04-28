2424b9ed4e6f69be9e453a2c73e2ba3d49f3b95b
Nikita Nyalapelli <85953042+NikitaNyalapelli29@users.noreply.github.com>
2022-04-28 20:26:07 +0530

    Create product of two numbers.java

---
+++ product of two numbers.java
@@ -0,0 +1,18 @@
+package basic;
+import java.util.Scanner;
+
+public class Practice {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Enter Value 1st : ");
+      int num1 = sc.nextInt();
+      System.out.println("\nEnter Value 2nd : ");
+      int num2 = sc.nextInt();
+      long multiplication = num1*num2;
+      System.out.println(multiplication);
+    }
+}
+
+      
+      
+      
