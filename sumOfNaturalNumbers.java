0c1aa435a2cd90888ba319907d857ab498bebd7b
Nikita Nyalapelli <85953042+NikitaNyalapelli29@users.noreply.github.com>
2022-10-05 16:42:51 +0530

    Create sumOfNaturalNumbers.java

---
+++ sumOfNaturalNumbers.java
@@ -0,0 +1,15 @@
+public class Sum_natural_number {
+    public static void main(String[] args){
+        int i,j,add = 0;
+        System.out.println("First 10 natural numbers are");
+        for(i=1;i<=10;i++){
+            System.out.println(i);
+            add+=i;
+
+        }
+
+
+        System.out.println("Sum of first 10 natural is "+add);
+
+    }
+}
