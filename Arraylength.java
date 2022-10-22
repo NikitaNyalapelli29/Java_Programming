fc2a3befa473013481b5b90963273544f8aea76f
Nikita Nyalapelli <85953042+NikitaNyalapelli29@users.noreply.github.com>
2022-10-22 17:16:08 +0530

    Create Arraylength.java

---
+++ Arraylength.java
@@ -0,0 +1,11 @@
+package basic;
+import java.util.Scanner;
+
+public class Array{
+  public static void main(String args[]){
+    String[] names = {"Pankaj","Dakshata","Shruti","Isha", "Nabil","Harsh","Sourabh","Prashant","Digvijay"};
+    System.out.print("Total Friends are:");
+    System.out.println(names.length);  
+  }
+}
+
