89a48af496dedd276604693325fa3951d6b0c6a9
Nikita Nyalapelli <85953042+NikitaNyalapelli29@users.noreply.github.com>
2022-10-22 17:00:39 +0530

    Create SimpleArray.java

---
+++ SimpleArray.java
@@ -0,0 +1,12 @@
+public class Array1 {
+    public static void main(String args[]){
+        int[] marks = new int[3];
+        marks[0] = 95;
+        marks[1]=92;
+        marks[2]=98;
+        //System.out.println(marks);  it provides garbage value
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+    }
+}
