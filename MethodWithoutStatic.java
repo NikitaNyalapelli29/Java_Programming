eb1f7cf6cafb7df0b0baf93c6419a9039d79a3b4
Nikita Nyalapelli <85953042+NikitaNyalapelli29@users.noreply.github.com>
2022-10-24 23:46:21 +0530

    Create MethodWithoutStatic.java

---
+++ MethodWithoutStatic.java
@@ -0,0 +1,16 @@
+package basic;
+
+public class Methods {
+    
+    int mult(int x, int y){
+        int z =x*y;
+        return z;
+    }
+    public static void main(String[] args) {
+
+        Methods obj = new Methods();
+        p = obj.mult(3,4);
+        System.out.println(p);
+
+    }
+}
