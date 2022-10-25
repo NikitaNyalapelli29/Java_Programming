fcc019e3b6f98482bfeb3a8987dcaa2ddbf1b47c
Nikita Nyalapelli <85953042+NikitaNyalapelli29@users.noreply.github.com>
2022-10-25 16:28:00 +0530

    Update TakingFromUserInputAverageMarksFromArray.java

--- TakingFromUserInputAverageMarksFromArray.java
+++ TakingFromUserInputAverageMarksFromArray.java
@@ -15,7 +15,7 @@ public class Array2 {
         for(int i:marks) {
             System.out.print(i +" ");
         }
-        for(int i=0;i< marks.length+1;i++){
+        for(int i:marks){
             sum = sum + i;
         }
         System.out.println();
