fa403a06afad1f26dce99fba90a78f6709833478
Nikita Nyalapelli <85953042+NikitaNyalapelli29@users.noreply.github.com>
2024-06-06 10:09:35 +0530

    Add files via upload

---
+++ CircumferenceCircle.java
@@ -0,0 +1,17 @@
+package Classwork;
+import java.util.*;
+public class CircumferenceCircle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter radius Value: ");
+		float radius = sc.nextFloat();
+		sc.close();
+		
+		
+		System.out.print("Circumference of a Circle: "+(2*3.14*radius));
+	}
+
+}
